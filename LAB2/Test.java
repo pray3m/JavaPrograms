cc87ca88ab49ebb137ebcee226f6f1def9eb8a27
Prem Gautam <prem.gtm9@gmail.com>
2022-08-19 16:27:49 +0545

    01 Java program to illustrate the application of literals

---
+++ LAB2/Test.java
@@ -0,0 +1,20 @@
+// Q.N-1 Java program to illustrate the application of literals
+public class Test {
+    public static void main(String[] args){
+        int a=101; //integer-form literal
+
+
+        float b= (float) 101.230; //decimal-form literal
+
+        char ch='a'; //single character literal with single quote
+        String s="Hello"; //String literals
+        boolean d=false; //boolean literals
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(ch);
+        System.out.println(s);
+        System.out.println(d);
+    }
+
+}
