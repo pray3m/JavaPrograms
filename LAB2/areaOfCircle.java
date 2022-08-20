fa622f372e33cf632908d8bf8cca04f899589b55
Prem Gautam <prem.gtm9@gmail.com>
2022-08-20 07:09:22 +0545

    area of circle

---
+++ LAB2/areaOfCircle.java
@@ -0,0 +1,11 @@
+// Q.N-3 Compute the area of circle
+public class areaOfCircle {
+    public static void main(String[] args) {
+        float a;
+        float pi=(float) 3.14;
+        int r=5;
+        a = pi * r * r; //compute area
+        System.out.println("Area of circle is : "+a);
+    }
+
+}
