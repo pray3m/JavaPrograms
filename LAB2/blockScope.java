282c8b8996068e24f84fff3948157196da30a70e
Prem Gautam <prem.gtm9@gmail.com>
2022-08-20 07:10:15 +0545

    demonstrate block scope

---
+++ LAB2/blockScope.java
@@ -0,0 +1,18 @@
+// Q.N-5. Run the following program to demonstrate the block scope.
+// Demonstrate block scope.
+
+class blockScope {
+    public static void main(String args[]) {
+        int x; // known to all code within main
+        x = 10;
+        if(x == 10) { // start new scope
+            int y = 20; // known only to this block
+// x and y both known here.
+            System.out.println("x and y: " + x + " " + y);
+            x = y * 2;
+        }
+        //y = 100; // Error! y not known here
+// x is still known here.
+        System.out.println("x is " + x);
+    }
+}
