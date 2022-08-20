6d7314240e8bc1edebc85c7053271f7321cfb599
Prem Gautam <prem.gtm9@gmail.com>
2022-08-20 07:10:58 +0545

    demonstrate boolean values

---
+++ LAB2/booleanTest.java
@@ -0,0 +1,18 @@
+// Q.N-4 Complete the following program in Java to demonstrate the Boolean values.
+
+public class booleanTest {
+    public static void main(String[] args){
+        boolean b;
+        b=false;
+        System.out.println("b is "+ b);
+        System.out.println("b is " + !b);
+
+       // TODO :Complete the Program
+
+        if(b) System.out.println("This is executed.");
+        b=false;
+        if(b) System.out.println("This is not executed.");
+        System.out.println("10 > 9 is "+(10>9));
+
+    }
+}
