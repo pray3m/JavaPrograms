decf2086e63d7f44076f3effe2b27b7bea03b81c
Prem Gautam <prem.gtm9@gmail.com>
2022-09-08 19:32:40 +0545

    calculate volume of 2 box objects

---
+++ LAB 3/BoxDemo.java
@@ -0,0 +1,34 @@
+/* 1. Complete the following a program in Java to create a class Box with instance variables length,
+width, and height. Create at least 2 objects from the class Box and calculate volume of each box
+object. */
+
+// this class declares an object of type box
+class Box{
+    int length;
+    int width;
+    int height;
+}
+
+public class BoxDemo {
+    public static void main(String[] args) {
+        Box box1=new Box();
+        Box box2=new Box();
+        double vol1,vol2;
+
+        //assign values of box instance variables
+        box1.length=6;
+        box1.height=7;
+        box1.width =8;
+
+        box2.length=12;
+        box2.width =43;
+        box2.height=3;
+
+        // compute volume
+        vol1= box1.length*box1.width *box1.height;
+        vol2= box2.length*box2.width *box2.height;
+
+        System.out.println("Volume of box1 is : "+vol1);
+        System.out.println("Volume of box2 is : "+vol2);
+    }
+}
---
+++ LAB 3/README.md
@@ -0,0 +1,11 @@
+
+# OOP in Java  - LAB 3
+
+### Objective:
+To learn concepts of object oriented paradigm in Java.
+
+## Acknowledgements
+
+ - [LAB 3 PDF](https://github.com/pray3m/JavaPrograms/blob/main/LAB3/lab%202(tokens%2C%20expressions%2C%20functions%2C%20array%2C%20conditioanls).pdf)
+ - [Q.N 1](https://github.com/pray3m/JavaPrograms/blob/main/LAB3/Test.java)
+- [Q.N 2](https://github.com/pray3m/JavaPrograms/blob/main/LAB3/light.java)
