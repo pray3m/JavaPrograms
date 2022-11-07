511301dca9777d75b13d77afe681ca3aedc9d7ef
anisha stha <stha.anisha16@gmail.com>
2022-11-07 14:05:00 +0545

    solved lab 7

--- LAB7/README.md
+++ LAB7/README.md
@@ -10,10 +10,10 @@ thread execution and Interthread communication.
  - [LAB 7 PDF](https://github.com/pray3m/JavaPrograms/blob/main/LAB7/lab%207-Thread.pdf)
  - [Q.N 1](https://github.com/pray3m/JavaPrograms/blob/main/LAB7/ThreadDemo.java)
  - [Q.N 2](https://github.com/pray3m/JavaPrograms/blob/main/LAB7/InterThreadCommunicatio.java)
- - [Q.N 3*](https://github.com/pray3m/JavaPrograms/blob/main/LAB7/link **)
- - [Q.N 4*](https://github.com/pray3m/JavaPrograms/blob/main/LAB7/link **)
- - [Q.N 5*](https://github.com/pray3m/JavaPrograms/blob/main/LAB7/link **)
- - [Q.N 6*](https://github.com/pray3m/JavaPrograms/blob/main/LAB7/link **)
+ - [Q.N 3](https://github.com/pray3m/JavaPrograms/blob/main/LAB7/Threadinterval.java)
+ - [Q.N 4](https://github.com/pray3m/JavaPrograms/blob/main/LAB7/Threadpriorites.java)
+ - [Q.N 5](https://github.com/pray3m/JavaPrograms/blob/main/LAB7/Runnableinterface.java)
+ - [Q.N 6](https://github.com/pray3m/JavaPrograms/blob/main/LAB7/Runnableinterfacedemo.java)
  
  
  
---
+++ LAB7/Runnableinterfacedemo.java
@@ -0,0 +1,34 @@
+/*  6. Create two classes ThreadA and ThreadB which implements Runnable interface. ThreadA
+displays all even numbers from 50 to 100 and Thread B displays all odd numbers from 100 to
+200. Define a main class which creates the objects of the both the classes and displays the
+numbers as per the above mentioned specifications. */
+
+class ThreadA implements Runnable{
+    public void run(){
+        for(int i=50;i<=100;i++){
+            if(i%2==0){
+                System.out.println("even no ="+i);
+            }
+        }
+    }
+}
+class ThreadB implements Runnable{
+    @Override
+    public void run() {
+        for(int j=100;j<=200;j++){
+            if(j%2==0){
+                System.out.println("Odd no="+j);
+            }
+        }
+    }
+}
+public class Runnableinterfacedemo {
+    public static void main(String[] args){
+        ThreadA a=new ThreadA();
+        Thread d=new Thread(a);
+        ThreadB b= new ThreadB();
+        Thread e= new Thread(b);
+        d.start();
+        e.start();
+    }
+}
